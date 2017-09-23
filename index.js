37a452a3de8e502afb3e201b6c5c4f6a9c9149b8
RyleeAndrews <ryleeandrews73@gmail.com>
2017-09-23 16:20:50 -0700

    all the files are being properly read. Now will be working on reading the files

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ fs.readFile(`${__dirname}/skiing.txt`, function(err,data){
     fs.readFile(`${__dirname}/hiking.txt`,function(err,data){
       if(err) throw err;
       console.log(err);
-      console.log('The contents');
+      console.log('The contents of my file is:', data.toString());
+    });
+  }
+  {
+    fs.readFile(`${__dirname}/pizza.txt`,function(err,data){
+      if(err) throw err;
+      console.log(err);
+      console.log('The contents of my file is:', data.toString());
     });
   }
 });
