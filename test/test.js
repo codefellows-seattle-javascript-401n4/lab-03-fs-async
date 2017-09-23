804957b295527b04957d5bf67fa0e2beeb2957d7
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-09-23 16:53:39 -0700

    done for saturday

--- test/test.js
+++ test/test.js
@@ -3,11 +3,14 @@
 const expect = require('expect');
 const fileReader = require('./../lib/reader.js');
 
+let textFiles = ['./../data/data.txt', './../data/data2.txt', './../data/data3.txt'];
+
 describe('fileReader', function(){
-  it('should read the .txt file give to it', function(){
-    fileReader('/../data/data.txt', (err,data) => {
+  it('should read the .txt file give to it', function(done){
+    fileReader(textFiles, (err,data) => {
       expect(err).toBe(null);
-      expect(data).toBe('derp');
+      expect(data).toBe('derp derp derp');
+      done();
     });
   });
 });
