0846a87b62523bbeda4e55143b199a7931c28435
meganr <meganrm@gmail.com>
2017-09-23 16:07:59 -0700

    refactored a bit to make sure everythign is in order. One method uses callbacks one uses promise

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const fileReader = require('./lib/print-files.js');
+
+
+//Using a promise
+fileReader.getFiles('/../data/file1.txt', '/../data/file2.txt', '/../data/file3.txt');
+//using a callback
+fileReader.getFilesCB(['/../data/file1.txt', '/../data/file2.txt', '/../data/file3.txt']);
