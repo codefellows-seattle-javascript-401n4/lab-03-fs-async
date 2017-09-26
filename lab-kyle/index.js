924b73ca48bfbe3710eda00b6df96ad478b40ca9
Kyle Richey <richey.kyle.i@gmail.com>
2017-09-25 22:56:43 -0700

    start of print-file module

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -1,1 +1,5 @@
 'use strict';
+
+const fs = require('fs');
+const printFiles = require(__dirname + '/lib/print-files.js');
+const expect = require('expect');
--- lab-kyle/lib/print-files.js
+++ lab-kyle/lib/print-files.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const fs = require('fs');
+
+module.exports = function (path, callback){
+  fs.readFile(path, utf8, callback);
+};
