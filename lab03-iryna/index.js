5a8f52f8081937150e9dadf84bd286b83228a69a
Iryna Maslova <maslovai@hotmail.com>
2017-09-26 20:52:23 -0700

    got file1 to print

--- lab03-iryna/index.js
+++ lab03-iryna/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 
-
 const readFiles = require('./lib/print-files.js');
+// const paths = ['/file1.txt', '/file2.txt', '/file3.txt'];
 
 let filesToPrint = function(data){
     console.log(data.toString());
 };
 
-readFiles('../lab03-iryna/lib/file1.txt', filesToPrint(data));
+readFiles('/file1.txt', function(err, data) {
+  // console.log(data.toString());
+  filesToPrint(data);
+});
--- lab03-iryna/lib/print-files.js
+++ lab03-iryna/lib/print-files.js
@@ -1,9 +1,10 @@
 'use strict';
+
 const fs = require('fs');
 
-let readFiles = module.exports  = (path, cb) => {
+module.exports = (path, cb) => {
   return fs.readFile(__dirname + path, (err, data)=>{
   if (err) return cb(err);
-  return cb(null, data);
+  return cb(null,data);
 });
 };
