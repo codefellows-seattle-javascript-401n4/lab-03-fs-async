e6d7645f7560619a1e7e4710ad12e9a13fa45a12
Kyle Richey <richey.kyle.i@gmail.com>
2017-10-29 13:50:18 -0700

    small edits

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -1,5 +1,4 @@
 'use strict';
 
 const fs = require('fs');
-const printFiles = require(__dirname + '/lib/print-files.js');
-const expect = require('expect');
+const printFiles = require('./lib/print-files.js');
